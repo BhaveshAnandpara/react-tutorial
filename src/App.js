486c630099e9aa2cb9084a8290982e495589e3cc
Bhavesh Anandpara <shivamcharde12@gmail.com>
2024-12-17 19:41:01 +0530

    use effect basic

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import Card from './component/Card';
-import { useState } from 'react';
-import NameCard from './component/NameCard';
-import Agecard from './component/Agecard';
+import React, { useEffect, useState } from 'react' //import useState Hook
+import './App.css'
 
-function App() {
+export default function App() {
 
-  const [name, setName] = useState("Bhavesh")   //This is useState so whenever this value changes the component that is using this value as prop will also RERENDER
-  const [age, setAge] = useState(22)   
-  let city = "Wardha"   //This is not a useState so even if this value changes the component will not RENDER.
+  const [count, setCount] = useState(0) //equivalent to count = 0
+
+
+  useEffect(() => {
+    alert(count)
+  }, [count])
+
+
+
+  function increment() {
+    console.log(count + 1); //conosle to check if the function works
+    setCount(count + 1) // equivalent to count = count+1
+    // alert(count)
+  }
 
   return (
     <>
-        <input type='text' placeholder='Enter Name here'   onChange={(e)=>setName(e.target.value)} />
-        <input type='number' placeholder='Enter Age here'  onChange={(e)=>setAge(e.target.value)}  />
-        <input type='text' placeholder='Enter city here'   onChange={(e)=>{ city=e.target.value } } />
 
-        <NameCard name={name}/>
-        <Agecard age={age} />
-        <NameCard name={city}/>
+      <div className="container">
+
+        <h2> {count} </h2>
+        <button className="btn" onClick={increment} > Increment by 1      </button>
+
+      </div>
+
     </>
   )
 }
 
-export default App;
